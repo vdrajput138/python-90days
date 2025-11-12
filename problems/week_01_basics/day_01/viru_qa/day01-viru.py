2a2a6544037c45adf11bd390376548aceb28f4a3
Virendra <vdrajput138@gmail.com>
2025-11-13 02:50:00 +0530

    Testing the another daily problem structure version

---
+++ problems/week_01_basics/day_01/viru_qa/day01-viru.py
@@ -0,0 +1,19 @@
+'''
+## Problems
+1. Install Python and `pytest` in a virtual env.
+2. Write a basic script printing 'QA Automation Setup OK'.
+3. Create a pytest test that asserts 2 + 2 == 4.
+4. Write and run a test file using `pytest -v`.
+5. Document how to install dependencies via `requirements.txt`.
+'''
+
+
+## True Assrt
+assert 2+2==4
+
+print("QA Automation Setup OK")
+
+## False Assert
+assert 2+2 == 5
+
+
