90c4b3b5da7982f5c32c537b8c035034fc30c630
Virendra <vdrajput138@gmail.com>
2025-11-14 01:12:02 +0530

    Added code of day 02 and day 03

--- Week01/Day04/Day04_viru.py
+++ Week01/Day04/Day04_viru.py
@@ -1,6 +1,4 @@
 """
-
-2. Write expressions using logical operators (and, or, not) and print their results
 3. Check if a number is between 1 and 100 (inclusive) using comparison operators
 4. Create a program that determines if a year is a leap year using boolean logic
 5. Write a function that validates a password based on multiple conditions (length, contains digit, contains special character)
@@ -18,3 +16,7 @@ print(f"We have {value_1} and {value_2} - {value_1 < value_2}")
 print(f"We have {value_1} and {value_2} - {value_1 > value_2}")
 print(f"We have {value_1} and {value_2} - {value_1 == value_2}")
 print(f"We have {value_1} and {value_2} - {value_1 != value_2}")
+
+"""
+2. Write expressions using logical operators (and, or, not) and print their results
+"""
