f07595dd5c7beb608b1527467442daf19b5446e5
Virendra <vdrajput138@gmail.com>
2025-11-17 01:03:19 +0530

    formatted code

---
+++ curriculum/Viru_QA_MLOps/Week_1/Day_1/day01.py
@@ -0,0 +1,7 @@
+"""Write a program that asks the user for their name and age, then prints:
+'Hello <name>, you are <age> years old.'
+"""
+
+name, age = input("Please enter your name and age. for example, Virendra 30").split()
+
+print("Hello " + name + ", you are " + age + " years old.")
