f08d96d5f52a927e56e8b6b61750527e6976826b
Virendra <vdrajput138@gmail.com>
2025-11-17 01:13:48 +0530

    validate the changes using black - again

--- problems/week_01_basics/day_01/viru_qa/day01-viru.py
+++ problems/week_01_basics/day_01/viru_qa/day01-viru.py
@@ -1,19 +1,16 @@
-'''
+"""
 ## Problems
 1. Install Python and `pytest` in a virtual env.
 2. Write a basic script printing 'QA Automation Setup OK'.
 3. Create a pytest test that asserts 2 + 2 == 4.
 4. Write and run a test file using `pytest -v`.
 5. Document how to install dependencies via `requirements.txt`.
-'''
-
+"""
 
 ## True Assrt
-assert 2+2==4
+assert 2 + 2 == 4
 
 print("QA Automation Setup OK")
 
 ## False Assert
-assert 2+2 == 5
-
-
+assert 2 + 2 == 5
